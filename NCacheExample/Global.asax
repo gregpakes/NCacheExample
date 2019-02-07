5c7d6dbb6302fefe33d5331d7fd8f8f57d8043a9
Greg Pakes <greg.pakes@isams.co.uk>
2019-02-08 09:54:27 +1100

    Initial Commit

---
+++ NCacheExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NCacheExample.MvcApplication" Language="C#" %>
---
+++ NCacheExample/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using NCacheExample;
+
+[assembly:PreApplicationStartMethod(typeof(ApplicationStart), "Startup")]
+
+namespace NCacheExample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+
+    public class ApplicationStart
+    {
+        public static void Startup()
+        {
+            // Perform application initialization here - in my case setup DI container and configure/prime cache
+            var cache = Alachisoft.NCache.Web.Caching.NCache.InitializeCache("myCache");
+        }
+    }
+}
